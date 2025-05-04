54ca8beef7d1c22182d16b2ad7ef41f818b299e0
jjxmonster <kuba.tarabasz@wp.pl>
2025-05-04 19:12:58 +0200

    chore: remove unused vars

--- env.mjs
+++ env.mjs
@@ -6,12 +6,8 @@ export const env = createEnv({
 		SUPABASE_URL: z.string(),
 		SUPABASE_KEY: z.string(),
 	},
-	client: {
-		NEXT_PUBLIC_SUPABASE_URL: z.string(),
-	},
 	runtimeEnv: {
 		SUPABASE_URL: process.env.SUPABASE_URL,
 		SUPABASE_KEY: process.env.SUPABASE_KEY,
-		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
 	},
 })
